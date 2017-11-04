26d2992df05a0fe80358bdf97bca30e9b035f661
Renan Arceno <r3n44n@gmail.com>
2017-11-03 22:26:02 -0200

    restoring models

--- hidro_neural.py
+++ hidro_neural.py
@@ -2,12 +2,15 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import os
+import shutil
 import itertools
 
 import pandas as pd
 import tensorflow as tf
 
 # tf.logging.set_verbosity(tf.logging.INFO)
+from tensorflow.python.framework.errors_impl import InvalidArgumentError
 
 COLUMNS = ["pessoas", "maquinas", "vazao_total", "vazao_1", "sensor_p1", "vazao_2", "sensor_p2", "vazao_3", "sensor_p3", "vazamento", "t_seg", "hora"]
 FEATURES_VAZ = ["pessoas", "maquinas", "vazao_total", "vazao_1", "sensor_p1", "vazao_2", "sensor_p2", "vazao_3", "sensor_p3", "vazamento"]
@@ -39,22 +42,23 @@ def predict_value(pessoas, maquinas, vazao_total, vazao_1, sensor_p1, vazao_2, s
     feature_cols = [tf.feature_column.numeric_column(k) for k in FEATURES]
 
     # Build 2 layer fully connected DNN with 30, 30 units respectively.
+    model_folder = "/tmp/hidro"
     regressor = tf.estimator.DNNRegressor(feature_columns=feature_cols,
-                                          hidden_units=[30, 30],
-                                          model_dir="/tmp/hidro")
+                                          hidden_units=[25, 30],
+                                          model_dir=model_folder)
 
-    # Train
-    regressor.train(input_fn=get_input_fn(training_set), steps=1000)
+    # Train if we dont have a trained model
+    if not os.path.isdir(model_folder) and not os.path.exists(model_folder):
+        regressor.train(input_fn=get_input_fn(training_set), steps=5000)
 
     # Evaluate loss over one epoch of test_set.
     ev = regressor.evaluate(input_fn=get_input_fn(test_set, num_epochs=1))
     loss_score = ev["loss"]
     print("Loss: {0:f}".format(loss_score))
 
-    # Print out predictions over a slice of prediction_set.
+    # Print out predictions
     y = regressor.predict(input_fn=predict_func)
     # .predict() returns an iterator of dicts; convert to a list and print
-    # predictions
-    predictions = list(p["predictions"] for p in itertools.islice(y, 10))
+    predictions = list(p["predictions"] for p in itertools.islice(y, 1))
     print("Predictions: {}".format(str(predictions[0][0])))
     return loss_score, predictions[0][0]
